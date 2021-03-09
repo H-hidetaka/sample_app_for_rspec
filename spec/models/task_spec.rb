2fe148a8dcfe452bc425a8cf9527f61b6c1bc77d
H-hidetaka <sansanhidetaka@gmail.com>
2021-03-10 08:56:51 +0900

    [Add] task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'validation' do
-    it 'is valid with all attributes' do end
+    it 'is valid with all attributes' do
+      task = build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
+
     it 'is invalid without title' do end
     it 'is invalid without status' do end
     it 'is invalid with a duplicate title' do end
