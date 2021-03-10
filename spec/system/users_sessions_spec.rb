f2142d1b3dc972d9ec454ff4defd613b2346cf98
H-hidetaka <sansanhidetaka@gmail.com>
2021-03-10 15:36:53 +0900

    [Add]users_sessinons_spec.rb change contents ver1

--- spec/system/users_sessions_spec.rb
+++ spec/system/users_sessions_spec.rb
@@ -6,16 +6,20 @@ RSpec.describe 'UserSessions', type: :system do
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
-        visit user_sessions_path
-        fill_in 'Email', with: 'email@examle.com'
+        visit login_path
+        fill_in 'Email', with: user.email
         fill_in 'Password', with: 'password'
-        fill_in 'Password_confirmation', with: 'password'
-        click_button 'SignIn'
-        expect(page).to have_content ''
-
+        click_button 'Login'
+        expect(page).to have_content 'Login successful'
+        expect(current_path).to eq root_path
+      end
     end
+
     context 'フォームが未入力' do
-      it 'ログイン処理が失敗する'
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
     end
   end
 
