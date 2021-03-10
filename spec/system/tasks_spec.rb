b3234c8827baf15cde54734522936ba19716b243
H-hidetaka <sansanhidetaka@gmail.com>
2021-03-10 16:28:27 +0900

    [Fix] tasks_spec.rb ver1

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
   let(:user) { create(:user) }
-  let(:user) { create(:task)}
+  let(:task) { create(:task)}
 
   describe 'ログイン前' do
     describe 'ページ遷移確認' do
@@ -10,9 +10,35 @@ RSpec.describe "Tasks", type: :system do
         it '新規登録ページへのアクセスが失敗する' do
           visit new_task_path
           click_button 'SignUp'
-          except(page).to have_content 'User was successfully created. '
+          except(page).to have_content('Login required')
           except(current_path).to eq login_path
         end
       end
-    end
-  end
+
+      context 'タスクの編集ページにアクセス' do
+        it '編集ページのアクセスが失敗する' do
+          visit edit_task_path
+          expect(page).to have_content task.title
+          expect(current_path).to eq new_task_path(task)
+        end
+      end
+
+      context 'タスクの編集ページにアクセス'　do
+        it '編集ページへのアクセスが失敗する'　do
+          visit edit_task_path(task)
+          expect(page).to have_content('Login required')
+          expect(current_path).to eq login_path
+        end
+      end
+
+      context 'タスクの詳細ページにアクセス' do
+        it 'タスクの詳細情報が表示される'
+          visit task_path(task)
+          expect(page).to have_content task.title
+          expect(current_path).to eq task_path(task)
+        end
+      end
+
+
+
+      end
