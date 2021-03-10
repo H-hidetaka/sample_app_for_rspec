ba5df340c21aac419d3bf69e68b881a5cecb362c
H-hidetaka <sansanhidetaka@gmail.com>
2021-03-10 16:22:55 +0900

    [Fix] tasks_spec.rb

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  let(:user) { create(:user) }
+  let(:user) { create(:task)}
 
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+  describe 'ログイン前' do
+    describe 'ページ遷移確認' do
+      context 'タスクの新規登録ページにアクセス' do
+        it '新規登録ページへのアクセスが失敗する' do
+          visit new_task_path
+          click_button 'SignUp'
+          except(page).to have_content 'User was successfully created. '
+          except(current_path).to eq login_path
+        end
+      end
+    end
+  end
